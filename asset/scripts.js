ef6515d50eb0b539cc7e4f2c6e597d68de0ba70f
JaQwaee <jaqwaeellison@gmail.com>
2022-07-16 13:14:35 -0500

    started a different direction on HTML file set font family, and some box prop in css reset

---
+++ asset/scripts.js
@@ -0,0 +1,16 @@
+var arrayOfQuestions = [
+    {
+        questions: 'Booleans have the following outputs',
+
+        answers: {
+            a: 'Yes/No',
+            b: 'True/False',
+            c: 'Right/Wrong',
+            d: 'None of the above',
+        },
+
+        correctAnswer: 'b'
+    }
+
+    
+]
