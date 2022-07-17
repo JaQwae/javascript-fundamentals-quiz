024fbfda755b52e291110a164ba6b66d1a485da2
JaQwaee <jaqwaeellison@gmail.com>
2022-07-16 19:54:17 -0500

    created a question, answer choices, and answer bank

--- asset/scripts.js
+++ asset/scripts.js
@@ -5,16 +5,120 @@ var arrayOfQuestions = [
     {
         questions: 'Which of the follow output pairs corresponds with booleans?',
 
-        answers: {
+        answers: [
             a: 'Yes/No',
             b: 'True/False',
             c: 'Right/Wrong',
             d: 'None of the above',
-        },
+        ],
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which of the follow is not a primitive type in Javascript?",
+        
+        answer: [
+            a:'Object',
+            b:'String',
+            c:'Number',
+            d:'Boolean',
+        ],
+
+        correctAnswer: 'a'
+    },
+    {
+        question: "Which of the follow is a 3rd party API for Javascript?",
+        
+        answer: [
+            a:'Event listener',
+            b:'DOM',
+            c:'CSS',
+            d:'JQuery',
+        ],
+
+        correctAnswer: 'd'
+    },
+    {
+        question: "All of the follow are loops in JS expect:",
+        
+        answer: [
+            a:'for/in',
+            b:'for',
+            c:'why',
+            d:'while',
+        ],
+
+        correctAnswer: 'c'
+    },
+    {
+        question: "Which is not considered a mouse event listener?",
+        
+        answer: [
+            a:'click',
+            b:'doubleClick',
+            c:'mousedown',
+            d:'mousemove',
+        ],
 
         correctAnswer: 'b'
-    }
+    },
+    {
+        question: "All of the follow are ways to declare a variable expect:",
+        
+        answer: [
+            a:'let',
+            b:'const',
+            c:'declar',
+            d:'var',
+        ],
 
+        correctAnswer: 'c'
+    },
+    {
+        question: "Say we have 5 === '5', will this default to true and why?",
+        
+        answer: [
+            a:'Yes, both have the say value',
+            b:'Yes, because the three equal forces everything to be equal',
+            c:'No, because the operator is looking for same type and value',
+            d:'No, because that is not a valid assignment operator',
+        ],
 
+        correctAnswer: 'c'
+    },
+    {
+        question: "When using the && assignment operator, what needs to be true for the conditional statement to evaluate to true?",
+        
+        answer: [
+            a:'The condition only on the left side of the operator needs to be true',
+            b:'Both conditions need to be true',
+            c:'The condition only on the right side of the operator needs to be true',
+            d:'All of the above',
+        ],
+
+        correctAnswer: 'b'
+    },
+    {
+        question: "When using the || assignment operator, what needs to be true for the conditional statement to evaluate to true?",
+        
+        answer: [
+            a:'The condition only on the left side of the operator needs to be true',
+            b:'The condition only on the right side of the operator needs to be true',
+            c:'Both conditions need to be true',
+            d:'All of the above',
+        ],
+
+        correctAnswer: 'd'
+    },
+    {
+        question: "What is an array?",
+        
+        answer: [
+            a:'An ordered list of values',
+            b:'A primitive type',
+            c:'An empty string',
+            d:'None of the above',
+        ],
+
+        correctAnswer: 'a'
+    },
 ]
-
